200a71dcb0ec60af23d5c7c43d91fccaedb67ac0
iuzul <iuzul@IBRAHIMUZUL>
2020-09-28 17:49:42 +0300

    117/118 Local Dosyadan Json Veri Okumak / FutureBuilder ile Json Verileri Listelemek

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_json_http/local_json.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Json ve Api"),),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(child:Text("Local Json"), color: Colors.green, onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LocalJsonKullanimi()));
+            },),
+          ],
+        ),
+      ),
+    );
+  }
+}
